8421d83790b657a91434a31ca1ee215025786958
Marina <marinaoiko19@gmail.com>
2026-07-24 13:37:58 +0200

    Move learning exercises out of tracked repo

--- 01_basics.py
+++
@@ -1,8 +0,0 @@
-name="Thanos"
-age=28
-height=1.78
-is_working=True
-
-print(name)
-print(age)
-print(type(age))
--- 08_api_call.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://api.github.com")
-print(response.status_code)   # 200 σημαίνει επιτυχία
-print(response.json())  
